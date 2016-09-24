f1000471ffac88b3bfacde8d8bb17a92976936e3
Kanishk Kunal <kanishk.kunal@hotmail.com>
2016-09-24 18:06:08 +0530

    Vuejs setup

--- scripts/App.vue
+++ scripts/App.vue
@@ -1,19 +1,21 @@
-<template>
-  <h1 class="red">{{msg}}</h1>
+<template lang="pug">
+  div
+    h1(class="title") {{msg}}
 </template>
 
 <script>
 export default {
   data () {
     return {
-      msg: 'Hello world!'
+      msg: 'Hello world! How are ya?'
     }
   }
 }
 </script>
 
-<style>
-  .red {
-    color: #f00;
+<style lang="scss">
+  .title {
+    color: #222;
+    text-align: center;
   }
 </style>
